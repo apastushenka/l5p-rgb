a1c1367d4fe57d58bf31834dd6a844b6829e0739
Alexander Pastushenka <a.pastushenka@gmail.com>
2022-12-27 15:37:28 +0400

    Use dialog for color picking

--- l5p-app/src/components/ColorZone.tsx
+++ l5p-app/src/components/ColorZone.tsx
@@ -1,4 +1,5 @@
-import { Popover, PopoverSurface, PopoverTrigger, useId } from '@fluentui/react-components';
+import { Button, Dialog, DialogActions, DialogBody, DialogContent, DialogSurface, DialogTrigger, makeStyles } from '@fluentui/react-components';
+import { useState } from 'react';
 import { ChromePicker } from 'react-color';
 import { Rgb } from '../color';
 
@@ -7,25 +8,68 @@ type ColorZoneProps = {
   onChange: (color: Rgb) => void;
 };
 
+const useStyles = makeStyles({
+  dialog: {
+    maxWidth: '214px',
+    paddingLeft: '6px',
+    paddingRight: '6px',
+    paddingTop: '6px',
+    paddingBottom: '6px',
+  }
+});
+
+const pickerStyle = {
+  default: {
+    picker: {
+      boxShadow: 'none',
+    },
+  }
+};
+
 function ColorZone({ color, onChange }: ColorZoneProps) {
-  let id = useId();
+  let [pickerOpen, setPickerOpen] = useState(false);
+  let [pickerColor, setPickerColor] = useState(color);
+
+  let classes = useStyles();
 
   let background = {
     backgroundColor: `rgb(${color.r}, ${color.g}, ${color.b})`,
   };
 
+  let onApply = () => {
+    setPickerOpen(false);
+    onChange(pickerColor);
+  };
+
+  let onOpenChange = (open: boolean) => {
+    setPickerOpen(open);
+    if (open) {
+      setPickerColor(color);
+    }
+  };
+
   return (
-    <Popover size='small' positioning={{ position: 'below', coverTarget: true }}>
-      <PopoverTrigger>
+    <Dialog open={pickerOpen} onOpenChange={(event, data) => onOpenChange(data.open)}>
+      <DialogTrigger>
         <div className='color-zone' style={background} />
-      </PopoverTrigger>
-      <PopoverSurface aria-labelledby={id}>
-        <div id={id}>
-          <ChromePicker color={color} disableAlpha={true}
-            onChange={(color) => onChange(color.rgb)} />
-        </div>
-      </PopoverSurface>
-    </Popover>
+      </DialogTrigger>
+      <DialogSurface className={classes.dialog}>
+        <DialogBody>
+          <DialogContent>
+            <div className='color-picker'>
+              <ChromePicker styles={pickerStyle} color={pickerColor} disableAlpha={true}
+                onChange={color => setPickerColor(color.rgb)} />
+            </div>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={onApply}>Apply</Button>
+            <DialogTrigger disableButtonEnhancement>
+              <Button>Cancel</Button>
+            </DialogTrigger>
+          </DialogActions>
+        </DialogBody>
+      </DialogSurface>
+    </Dialog>
   );
 }
 
